81e2f1603ed67c1a54412057665f9156fcb8cbe0
Maciej Skierkowski <maciej@skierkowski.com>
2025-05-09 09:53:22 -0700

    adding slack

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,6 +2,27 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
+import { registerApiRoute } from "@mastra/core/server";
+
+import { SlackMastraAdapter } from '@serverless/ai';
+
+const adapterConfig = {
+  slack: {
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+  },
+};
+
+const slackAdapter = new SlackMastraAdapter(weatherAgent, adapterConfig);
+
+/**
+ * When SocketMode is disabled, we use the SlackMastraAdapter to register the
+ * API Route with the Mastra server. If SocketMode is enabled, we don't add
+ * any routes.
+ */
+const apiRoutes = [registerApiRoute("/slack", (slackAdapter as SlackMastraAdapter).route)];
+
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
@@ -10,4 +31,7 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
+  server: {
+    apiRoutes,
+  },
 });
